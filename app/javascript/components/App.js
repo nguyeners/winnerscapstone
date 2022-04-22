198248621f4d91a73f6569069fece534a6f1f354
Paula Nguyen <nguyen.paula5560@gmail.com>
2022-04-22 15:24:19 -0700

    worked on conditional rendering. Was able to set up and add edit to the nav bar only when signed in.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -15,22 +15,9 @@ import {
 
 class App extends Component {
   render() {
-    const {
-      logged_in,
-      current_user,
-      new_user_route,
-      sign_in_route,
-      sign_out_route
-    } = this.props
-    console.log("logged_in:", logged_in)
-    console.log("current_user:", current_user)
-    console.log("new_user_route:", new_user_route)
-    console.log("sign_in_route:", sign_in_route)
-    console.log("sign_out_route:", sign_out_route)
     return(
-
       <Router>
-      <Header />
+      <Header {...this.props}/>
         <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/EventIndex' component={EventIndex} />
