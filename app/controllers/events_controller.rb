c3b7df6c8326d2c492d1da82b3f3d89a49dc07a2
Kendra West <dualwieldingbucklers@gmail.com>
2022-04-25 16:27:42 -0700

    worked on controller a bit, more to come

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,52 @@
 class EventsController < ApplicationController
+
+  def index
+    events = Event.all
+    render json: events
+  end
+
+  def show
+    event = Event.find(params[:id])
+    render json: event
+  end
+
+  def create
+    if !current_user 
+      render json: {status: 422, error: "Must be logged in"}
+    else
+      event = user.events.create(event_params)
+      render json: event
+    end
+  end
+
+  def update
+    # if the event belongs to the current user, allow them to update it
+    if current_user
+    end
+    event = user.events.update(event_params)
+    render json: event
+  end
+  
+  def destroy
+    #if the event belongs to the current user, allow them to destroy it
+    if current_user
+    end
+    event = user.events.destroy
+  end
+
+  def show
+  end
+  #############
+  private
+  ##############
+  def event_params
+    params.require(:event).permit(:image, :category, :about, :event_name)
+  end
+
+  def authorize_item
+    unless @item.user == current_user 
+      redirect_to items_path, error: 'You are not authorized'
+    end
+  end
+
 end
