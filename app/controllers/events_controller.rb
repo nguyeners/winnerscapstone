1ab41fcfa6854581c9cb21c159c737a3fb1b08a9
Kendra West <dualwieldingbucklers@gmail.com>
2022-04-26 10:04:31 -0700

    more changes to endpoints

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,32 +19,40 @@ class EventsController < ApplicationController
     end
   end
 
+  #
   def update
-    # if the event belongs to the current user, allow them to update it
-    if current_user
-    end
+    event = Event.find(params[:id])
+    #if current_user created event
+      event.update
+      render json: event
+    #end
     event = user.events.update(event_params)
     render json: event
   end
   
+  #
   def destroy
-    #if the event belongs to the current user, allow them to destroy it
-    if current_user
-    end
-    event = user.events.destroy
+    event = Event.find(params[:id])
+    #if current_user created event
+      event.destroy
+    else
+      render json: {status: 422, error: "Must be logged in"}
+    #end
   end
 
   def show
   end
-  #############
+
+  ########
   private
-  ##############
+  ########
+
   def event_params
     params.require(:event).permit(:image, :category, :about, :event_name)
   end
 
   def authorize_item
-    unless @item.user == current_user 
+    unless event.user == current_user 
       redirect_to items_path, error: 'You are not authorized'
     end
   end
