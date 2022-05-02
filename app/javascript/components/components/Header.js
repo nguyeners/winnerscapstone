621df690be76104e3cb8740be9602097a4c8ecd3
Omar Bowen <bronxkidd95@gmail.com>
2022-05-02 08:57:53 -0700

    styling on about us page and header

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -15,6 +15,7 @@ class Header extends Component {
       <div className="header" id = 'full-page'>
         <h1 className="welcome">TEAMLY</h1>
         <h2 className="welcome" align="right">Hello {logged_in ? this.props.current_user.email : 'guest'}!</h2>
+        <img src= "https://images.unsplash.com/photo-1569517282132-25d22f4573e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1866&q=80" height="100px" width="100px"/>
         <Nav>
           <NavItem>
             <NavLink href="/">Home</NavLink>
--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -7,27 +7,29 @@ class AboutUs extends Component {
   render() {
     return(
     <>
+    <div className="body">
     <h3> Get to know the Founders </h3>
     <div className="container">
       <img className="container__image"
-      src={kendra} id='box' height = '300px' width = '200px'/>
+      src={kendra} id='box' height = '300px' width = '250px'/>
       <div className="container__text">
-      <h6> Kendra's Bio.</h6>
+      <h6> Kendra's Bio. </h6>
       </div>
     </div>
     <div className="container2">
       <img className="container__image2"
-      src={portfoliopic} id='box' height = '200px' width = '200px'/>
+      src={portfoliopic} id='box' height = '300px' width = '250px'/>
       <div className="container__text2">
-      <h6> Growing up playing competitive sports I’ve always had the passion to compete. As a adult, it I’ve seen how hard it can be to find other people that want to play and thought Teamly would be the perfect platform to help solve that for everyone.</h6>
+      <h6> Growing up playing sports Omar always had a passion for playing and competing with others. Throughout high school, college and the military he’s played in numerous Basketball, Football, Track, Soccer, Frisbee and Volleyball leagues and teams. His background and shared passion to play sports led him and the other cofounders to create Teamly.</h6>
       </div>
     </div>
-    <div className="container">
-      <img className="container__image"
-      src={paulapic} id='box' height = '300px' width = '200px'/>
-      <div className="container__text">
+    <div className="container3">
+      <img className="container__image3"
+      src={paulapic} id='box' height = '300px' width = '250px'/>
+      <div className="container__text2">
       <h6> Paula's Bio.</h6>
       </div>
+    </div>
     </div>
     </>
     )
